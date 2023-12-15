f4aec81b8a7a928fd6dee183a3e052338019212d
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-15 15:14:47 +0530

    added new methods

--- Getri_API_OnionArchitecture/Controllers/UserController.cs
+++ Getri_API_OnionArchitecture/Controllers/UserController.cs
@@ -71,6 +71,7 @@ namespace Getri_API_OnionArchitecture.Controllers
             userEntity.ModifiedDate = DateTime.UtcNow;
             userEntity.IPAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString();
             userEntity.Profile = new UserProfile();
+            userEntity.Profile.Id = model.Id;
             userEntity.Profile.FirstName = model.Firstname;
             userEntity.Profile.LastName = model.Lastname;
             userEntity.Profile.Address = model.Address;
@@ -81,5 +82,59 @@ namespace Getri_API_OnionArchitecture.Controllers
             iUserRepository.UpdateUser(userEntity);
             return 1;
         }
-    }
+
+        [HttpDelete("DeleteUser")]
+        public int DeleteUser(Int64 Id)
+        {
+            iUserRepository.DeleteUser(Id);
+            return 1;
+        }
+
+        [HttpGet("GetUserById")]
+        public IActionResult GetUserById(Int64 Id)
+        {
+            User user = new User();
+            user = iUserRepository.Getuser(Id);
+            UserProfile userProfile = iUserProfileRepository.GetUserProfile(Id);           
+            user.Profile = new UserProfile();
+            user.Profile.Id = userProfile.Id;
+            user.Profile.FirstName = userProfile.FirstName;
+            user.Profile.LastName = userProfile.LastName;
+            user.Profile.Address = userProfile.Address;
+            user.Profile.ContactNo = userProfile.ContactNo;
+            user.Profile.ModifiedDate = userProfile.ModifiedDate;
+            user.Profile.IPAddress = userProfile.IPAddress;
+            return Ok(user);
+        }
+
+        [HttpGet("GetAllUsers")]
+        public IActionResult GetAllUsers()
+        {
+            List<User> lstUser = new List<User>();
+            var lstUsers = iUserRepository.GetUsers().ToList();
+
+            foreach (var item in lstUsers)
+            {
+                User user = new User();
+                UserProfile userProfile = iUserProfileRepository.GetUserProfile(item.Id);
+                user.Id = item.Id;
+                user.UserName = item.UserName;
+                user.Password = item.Password;
+                user.Email = item.Email;
+                user.ModifiedDate = item.ModifiedDate;
+                user.IPAddress = item.IPAddress;
+                user.Profile = new UserProfile();
+                user.Profile.Id = userProfile.Id;
+                user.Profile.FirstName = userProfile.FirstName;
+                user.Profile.LastName = userProfile.LastName;
+                user.Profile.Address = userProfile.Address;
+                user.Profile.ContactNo = userProfile.ContactNo;
+                user.Profile.ModifiedDate = userProfile.ModifiedDate;
+                user.Profile.IPAddress = userProfile.IPAddress;
+                lstUser.Add(user);
+            }
+
+            return Ok(lstUser);
+        }
+    }    
 }
